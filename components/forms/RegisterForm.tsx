33701c53ebc4676de08a0945c91511feeb5bbf24
real-master-100 <albertofernandez8978@gmail.com>
2024-08-14 13:52:02 +0500

    .

---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,366 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl } from "@/components/ui/form";
+import CustomFormField from "../ui/CustomFormField";
+import SubmitButton from "../ui/SubmitButton";
+import { useState } from "react";
+import { PatientFormValidation, UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUser, registerPatient } from "@/lib/actions/patient.actions";
+import { FormFieldType } from "./PatientForm";
+import { RadioGroup } from "@radix-ui/react-radio-group";
+import {
+  Doctors,
+  GenderOptions,
+  IdentificationTypes,
+  PatientFormDefaultValues,
+} from "@/constants";
+import { RadioGroupItem } from "../ui/radio-group";
+import { Label } from "../ui/label";
+import { SelectItem } from "../ui/select";
+import Image from "next/image";
+import FileUploader from "../ui/FileUploader";
+
+const RegisterForm = ({ user }: { user: User }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof PatientFormValidation>>({
+    resolver: zodResolver(PatientFormValidation),
+    defaultValues: {
+      ...PatientFormDefaultValues,
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof PatientFormValidation>) {
+    setIsLoading(true);
+
+    let formData;
+
+    if(values.identificationDocument && values.identificationDocument.length > 0) {
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      });
+
+      formData = new FormData();
+      formData.append('blobFile', blobFile);
+      formData.append('filename', values.identificationDocument[0].name)
+    }
+
+    try {
+        const patientData = {
+          ...values,
+          userId: user.$id,
+          birthData: new Date(values.birthDate),
+          identificationDocument: formData,
+        }
+
+        // @ts-ignore
+        const patient = await registerPatient(patientData);
+
+        if(patient) router.push(`/patients/${user.$id}/new-appointment`)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-12 flex-1"
+      >
+        <section className="space-y-4">
+          <h1 className="header">Welcome 👋</h1>
+          <p className="text-dark-700">Tell us more about yourself.</p>
+        </section>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Personal Information</h2>
+          </div>
+        </section>
+
+        {/* Full Name Field */}
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* Email Field */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="johndoe@gmail.com"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
+
+          {/* Phone Number Field */}
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone Number"
+            placeholder="+923365861212"
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* DoB Field */}
+          <CustomFormField
+            fieldType={FormFieldType.DATE_PICKER}
+            control={form.control}
+            name="birthDate"
+            label="Data of Birth"
+          />
+
+          {/* Gender Field */}
+          <CustomFormField
+            fieldType={FormFieldType.SKELETON}
+            control={form.control}
+            name="gender"
+            label="Gender"
+            renderSkeleton={(field) => (
+              <FormControl>
+                <RadioGroup
+                  className="flex h-11 gap-6 xl:justify-between"
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  {GenderOptions.map((option) => (
+                    <div key={option} className="radio-group">
+                      <RadioGroupItem value={option} id={option} />
+                      <Label htmlFor={option} className="cursor-pointer">
+                        {option}
+                      </Label>
+                    </div>
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* Address Field */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="address"
+            label="Address"
+            placeholder="location address..."
+          />
+
+          {/* Occupation Field */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="occupation"
+            label="Occupation"
+            placeholder="occupation... e.g. Doctor"
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* Emergency Contact Name */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="emergencyContactName"
+            label="Emergency contact name"
+            placeholder="Guardian's Name..."
+          />
+
+          {/* Emergency Contact Number */}
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="emergencyContactNumber"
+            label="Emergency contact number"
+            placeholder="(555) 123-4567"
+          />
+        </div>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical Information</h2>
+          </div>
+        </section>
+
+        {/* Primary Physicial Select Field */}
+        <CustomFormField
+          fieldType={FormFieldType.SELECT}
+          control={form.control}
+          name="primaryPhysician"
+          label="Primary Physician"
+          placeholder="Select a physician"
+        >
+          {Doctors.map((doctor) => (
+            <SelectItem key={doctor.name} value={doctor.name}>
+              <div className="flex cursor-pointer items-center gap-2">
+                <Image
+                  src={doctor.image}
+                  width={32}
+                  height={32}
+                  alt={doctor.name}
+                  className="rounded-full border border-dark-500"
+                />
+                <p>{doctor.name}</p>
+              </div>
+            </SelectItem>
+          ))}
+        </CustomFormField>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* Insurance Provider Field */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="insuranceProvider"
+            label="Insurance provider"
+            placeholder="e.g. Kamal Insurance"
+          />
+
+          {/* Insurance Number Field */}
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="insurancePolicyNumber"
+            label="Insurance policy number"
+            placeholder="Insurance Number..."
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* Allergies Field */}
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="allergies"
+            label="Allergies (if any)"
+            placeholder="Peanuts, Penicillin etc..."
+          />
+
+          {/* Insurance Number Field */}
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="currentMedication"
+            label="Current medication (if any)"
+            placeholder="e.g. ibuprofen 200mg..."
+          />
+        </div>
+
+        <div className="flex flex-col gap-6 xl:flex-row">
+          {/* Fam Medical Hist */}
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="familyMedicalHistory"
+            label="Family medical history"
+            placeholder="e.g. mother had heart disease."
+          />
+
+          {/* Insurance Number Field */}
+          <CustomFormField
+            fieldType={FormFieldType.TEXTAREA}
+            control={form.control}
+            name="pastMedicalHistory"
+            label="Past medical history"
+            placeholder="e.g. Appendetomy, Tonsillectomy... "
+          />
+        </div>
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Identification and Verification</h2>
+          </div>
+        </section>
+
+        {/* Identification Select Field */}
+        <CustomFormField
+          fieldType={FormFieldType.SELECT}
+          control={form.control}
+          name="identificationType"
+          label="Identification type"
+          placeholder="Select identification type"
+        >
+          {IdentificationTypes.map((type) => (
+            <SelectItem key={type} value={type}>
+              {type}
+            </SelectItem>
+          ))}
+        </CustomFormField>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="identificationNumber"
+          label="Identification number"
+          placeholder="e.g. 123456789"
+        />
+
+        {/* Upload Identification Document Field */}
+        <CustomFormField
+          fieldType={FormFieldType.SKELETON}
+          control={form.control}
+          name="identificationDocument"
+          label="Scanned copy of identification document"
+          renderSkeleton={(field) => (
+            <FormControl>
+              <FileUploader files={field.value} onChange={field.onChange} />
+            </FormControl>
+          )}
+        />
+
+        <section className="space-y-6">
+          <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Consent and Privacy</h2>
+          </div>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="treatmentConsent"
+          label="I consent to treatment for my health condition"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="disclosureConsent"
+          label="I consent to the use and disclosure of my health information for treatment purposes."
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.CHECKBOX}
+          control={form.control}
+          name="privacyConsent"
+          label="I acknowledge that i have reviewed and agree to the privacy policy."
+        />
+
+        <SubmitButton isLoading={isLoading}>Submit and continue</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,84 @@
+"use server"
+
+import { ID, Query } from "node-appwrite";
+import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+import {InputFile} from "node-appwrite/file"
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    console.log("Creating user with data:", user);
+    
+    // Logging the endpoint to ensure it's correctly set
+    console.log("Appwrite Endpoint:", process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT);
+    
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+    console.log("New user created:", newUser);
+
+    return parseStringify(newUser);
+  } catch (error: any) {
+    console.error("Error creating user:", error);
+
+    if (error && error.code === 409) {
+      try {
+        const documents = await users.list([
+          Query.equal("email", [user.email]),
+        ]);
+        console.log("Existing user found:", documents.users[0]);
+
+        return documents.users[0];
+      } catch (listError: any) {
+        console.error("Error listing users:", listError);
+        throw listError;
+      }
+    }
+
+    throw error;
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const user = await users.get(userId)
+
+    return parseStringify(user);
+  } catch (error: any) {
+    console.log(error)
+  }
+}
+
+export const registerPatient = async({identificationDocument, ...patient}: RegisterUserParams) => {
+  try {
+    let file;
+
+    if(identificationDocument){
+      const inputFile = InputFile.fromBuffer(
+        identificationDocument?.get('blobFile') as Blob,
+        identificationDocument?.get('fileName') as string,
+      )
+
+      file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile)
+    }
+
+    const newPatient = await databases.createDocument(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      ID.unique(),
+      {
+        identificationDocumentID: file?.$id || null,
+        identificationDocumentUrl: `${ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+        ...patient
+      }
+    )
+
+  } catch (error) {
+    console.log(error);
+  }
+}
